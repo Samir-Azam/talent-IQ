54ae1640cb7b77418457efe9848d1b78bb7f9b4d
Samir Azam <azamsamir190@gmail.com>
2026-01-24 15:01:20 +0530

    initial commit: clean project setup

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { ENV } from './lib/env.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' })
+})
+
+app.listen(ENV.PORT, () => console.log('Server running on port: ${ENV.PORT}'))
